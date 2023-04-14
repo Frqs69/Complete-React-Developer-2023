cc6f73a4e77025f6cbdb99ddbd9a3074fd3848a5
Kamil <95419657+Frqs69@users.noreply.github.com>
2023-04-14 19:53:50 +0200

    Create navigation and routing

--- ReactBeginnin/src/App.jsx
+++ ReactBeginnin/src/App.jsx
@@ -1,37 +1,16 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import CategoryContainer from "./components/category-container/category-container.component";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
+// index - if you hit route '/' it will be rendered as base component
 function App() {
-	const categories = [
-		{
-			id: 1,
-			title: "hats",
-			imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-		},
-		{
-			id: 2,
-			title: "jackets",
-			imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-		},
-		{
-			id: 3,
-			title: "sneakers",
-			imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-		},
-		{
-			id: 4,
-			title: "womens",
-			imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-		},
-		{
-			id: 5,
-			title: "mens",
-			imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-		},
-	];
-
-	return <CategoryContainer categories={categories} />;
+	return (
+		<Routes>
+			<Route path='/' element={<Navigation />}>
+				<Route index element={<Home />} />
+			</Route>
+		</Routes>
+	);
 }
-
 export default App;
