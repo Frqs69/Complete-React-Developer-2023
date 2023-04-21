b43cb9adf6f0dcd87b03faa24754caa3631e334b
Kamil <95419657+Frqs69@users.noreply.github.com>
2023-04-21 12:07:30 +0200

    Created first styled component
    Start to use styled components in React

--- ReactBeginnin/src/components/directory-item/directory-item.component.jsx
+++ ReactBeginnin/src/components/directory-item/directory-item.component.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 import "./directory-item.styles.scss";
 
 const DirectoryItem = ({ category }) => {
 	const { imageUrl, title } = category;
+	const navigate = useNavigate();
+
+	const handleNavigate = () => {
+		navigate(`shop/${title}`);
+	};
+
 	return (
 		<div className='directory-item-container'>
 			<div
@@ -10,7 +18,7 @@ const DirectoryItem = ({ category }) => {
 					backgroundImage: `url(${imageUrl})`,
 				}}></div>
 
-			<div className='directory-item-body-container'>
+			<div className='directory-item-body-container' onClick={handleNavigate}>
 				<h2>{title}</h2>
 				<p>Shop Now</p>
 			</div>
