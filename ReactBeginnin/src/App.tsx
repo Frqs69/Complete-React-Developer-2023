b33162344f8ea823203aba4c80110823f256f9dc
Kamil <95419657+Frqs69@users.noreply.github.com>
2023-05-19 17:53:07 +0200

    Added lazy loading and suspense with spinner

--- ReactBeginnin/src/App.tsx
+++ ReactBeginnin/src/App.tsx
@@ -1,17 +1,30 @@
 import { Routes, Route } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
 
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/checkout/checkout.component";
+// import Home from "./routes/home/home.component";
+// import Navigation from "./routes/navigation/navigation.component";
+// import Authentication from "./routes/authentication/authentication.component";
+// import Shop from "./routes/shop/shop.component";
+// import Checkout from "./routes/checkout/checkout.component";
 import {
 	onAuthStateChangedListener,
 	createUserDocumentFromAuth,
 } from "./utils/firebase/firebase.utils";
 import { setCurrentUser } from "./store/user/user.reducer";
+import Spinner from "./components/spinner/spinner.component";
+
+// added lazy loading for all routes
+// when they are loading spinner will appear and disappear when content will be loaded
+const Home = lazy(() => import("./routes/home/home.component"));
+const Authentication = lazy(
+	() => import("./routes/authentication/authentication.component")
+);
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
+const Navigation = lazy(
+	() => import("./routes/navigation/navigation.component")
+);
 
 // index - if you hit route '/' it will be rendered as base component
 function App() {
@@ -30,14 +43,16 @@ function App() {
 	}, []);
 
 	return (
-		<Routes>
-			<Route path='/' element={<Navigation />}>
-				<Route index element={<Home />} />
-				<Route path='auth' element={<Authentication />} />
-				<Route path='shop/*' element={<Shop />}></Route>
-				<Route path='checkout' element={<Checkout />} />
-			</Route>
-		</Routes>
+		<Suspense fallback={<Spinner />}>
+			<Routes>
+				<Route path='/' element={<Navigation />}>
+					<Route index element={<Home />} />
+					<Route path='auth' element={<Authentication />} />
+					<Route path='shop/*' element={<Shop />}></Route>
+					<Route path='checkout' element={<Checkout />} />
+				</Route>
+			</Routes>
+		</Suspense>
 	);
 }
 export default App;
