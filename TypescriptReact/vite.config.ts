d702d87df47f68a86894b9353b9550f48fbaa4d2
Kamil <95419657+Frqs69@users.noreply.github.com>
2023-05-30 20:28:56 +0200

    added pwa

--- TypescriptReact/vite.config.ts
+++ TypescriptReact/vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: "autoUpdate",
+			manifest: {
+				name: "My Awesome App",
+				short_name: "MyApp",
+				description: "My Awesome App description",
+				theme_color: "#ffffff",
+        display: 'standalone',
+        scope:'/',
+        start_url: '/',
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
+});
